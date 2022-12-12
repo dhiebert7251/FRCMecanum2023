699366fab5090894f8d8f22ceb856af142c93d7f
dhiebert7251 <dan.hiebert@sbcglobal.net>
2022-12-12 09:37:53 -0600

    temp data in telemetry subsystem

--- src/main/java/frc/robot/subsystems/Telemetry.java
+++ src/main/java/frc/robot/subsystems/Telemetry.java
@@ -12,41 +12,42 @@ public class Telemetry extends SubsystemBase {
   /** Creates a new Telemetry. */
   public Telemetry() {
 
-
+;
   }
 
   @Override
   public void periodic() {
-    public final PowerDistribution pdp = new PowerDistribution();
-    // This method will be called once per scheduler run
-        // Get the current going through channel 7, in Amperes.
-    // The PDP returns the current in increments of 0.125A.
-    // At low currents the current readings tend to be less accurate.
-    double current7 = pdp.getCurrent(7);
-    SmartDashboard.putNumber("Current Channel 7", current7);
+    try (PowerDistribution pdp = new PowerDistribution()) {
+      // This method will be called once per scheduler run
+          // Get the current going through channel 7, in Amperes.
+      // The PDP returns the current in increments of 0.125A.
+      // At low currents the current readings tend to be less accurate.
+      double current7 = pdp.getCurrent(7);
+      SmartDashboard.putNumber("Current Channel 7", current7);
 
-    // Get the voltage going into the PDP, in Volts.
-    // The PDP returns the voltage in increments of 0.05 Volts.
-    double voltage = pdp.getVoltage();
-    SmartDashboard.putNumber("Voltage", voltage);
+      // Get the voltage going into the PDP, in Volts.
+      // The PDP returns the voltage in increments of 0.05 Volts.
+      double voltage = pdp.getVoltage();
+      SmartDashboard.putNumber("Voltage", voltage);
 
-    // Retrieves the temperature of the PDP, in degrees Celsius.
-    double temperatureCelsius = pdp.getTemperature();
-    SmartDashboard.putNumber("Temperature", temperatureCelsius);
+      // Retrieves the temperature of the PDP, in degrees Celsius.
+      double temperatureCelsius = pdp.getTemperature();
+      SmartDashboard.putNumber("Temperature", temperatureCelsius);
 
-    // Get the total current of all channels.
-    double totalCurrent = pdp.getTotalCurrent();
-    SmartDashboard.putNumber("Total Current", totalCurrent);
+      // Get the total current of all channels.
+      double totalCurrent = pdp.getTotalCurrent();
+      SmartDashboard.putNumber("Total Current", totalCurrent);
 
-    // Get the total power of all channels.
-    // Power is the bus voltage multiplied by the current with the units Watts.
-    double totalPower = pdp.getTotalPower();
-    SmartDashboard.putNumber("Total Power", totalPower);
+      // Get the total power of all channels.
+      // Power is the bus voltage multiplied by the current with the units Watts.
+      double totalPower = pdp.getTotalPower();
+      SmartDashboard.putNumber("Total Power", totalPower);
 
-    // Get the total energy of all channels.
-    // Energy is the power summed over time with units Joules.
-    double totalEnergy = pdp.getTotalEnergy();
-    SmartDashboard.putNumber("Total Energy", totalEnergy);
+      // Get the total energy of all channels.
+      // Energy is the power summed over time with units Joules.
+      double totalEnergy = pdp.getTotalEnergy();
+      SmartDashboard.putNumber("Total Energy", totalEnergy);
+    }
 
   }
 }
